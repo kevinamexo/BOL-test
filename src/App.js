5b94a39f67cd58ba9ad9bb0d0bb70c7289258fa2
Kevin Amexo <bangitskev@gmail.com>
2024-02-16 14:13:33 +0000

    feat(modal): Implement Modal for movie details and trailer

--- src/App.js
+++ src/App.js
@@ -8,20 +8,19 @@ import Header from './components/Header'
 import Movies from './components/Movies'
 import Starred from './components/Starred'
 import WatchLater from './components/WatchLater'
-import YouTubePlayer from './components/YoutubePlayer'
+import Modal from './components/Modal'
 import './app.scss'
 
 const App = () => {
 
-  const state = useSelector((state) => state)
+  const { movieTrailerModal, videoKey } = useSelector((state) => state.modal);
   const dispatch = useDispatch()
   const [searchParams, setSearchParams] = useSearchParams()
   const searchQuery = searchParams.get('search')
-  const [videoKey, setVideoKey] = useState()
   const [isOpen, setOpen] = useState(false)
   const navigate = useNavigate()
   const {searchResults, movies} =useSelector(state=>state.movies)
-  
+
   
   const closeModal = () => setOpen(false)
   
@@ -52,24 +51,7 @@ const App = () => {
     }
   }
 
-  const viewTrailer = (movie) => {
-    getMovie(movie.id)
-    if (!videoKey) setOpen(true)
-    setOpen(true)
-  }
-
-  const getMovie = async (id) => {
-    const URL = `${ENDPOINT}/movie/${id}?api_key=${API_KEY}&append_to_response=videos`
-
-    setVideoKey(null)
-    const videoData = await fetch(URL)
-      .then((response) => response.json())
-
-    if (videoData.videos && videoData.videos.results.length) {
-      const trailer = videoData.videos.results.find(vid => vid.type === 'Trailer')
-      setVideoKey(trailer ? trailer.key : videoData.videos.results[0].key)
-    }
-  }
+  
 
   useEffect(() => {
     console.log('START FETCH')
@@ -77,22 +59,19 @@ const App = () => {
   }, [])
 
   return (
-    <div className="App">
+    <div className={`App ${movieTrailerModal ===true && "full"}`}>
       <Header searchMovies={searchMovies} searchParams={searchParams} setSearchParams={setSearchParams} />
 
       <div className="container">
-        {videoKey ? (
-          <YouTubePlayer
-            videoKey={videoKey}
-          />
-        ) : (
-          <div style={{padding: "30px"}}><h6>no trailer available. Try another movie</h6></div>
-        )}
-
+         {movieTrailerModal&&(
+          <div className="modal-container">
+              <Modal videoKey={videoKey}/>
+          </div>
+          )}
         <Routes>
-          <Route path="/" element={<Movies movies={searchResults.length?searchResults:movies} viewTrailer={viewTrailer} closeCard={closeCard} />} />
-          <Route path="/starred" element={<Starred viewTrailer={viewTrailer} />} />
-          <Route path="/watch-later" element={<WatchLater viewTrailer={viewTrailer} />} />
+          <Route path="/" element={<Movies movies={searchResults.length?searchResults:movies}  closeCard={closeCard} />} />
+          <Route path="/starred" element={<Starred  />} />
+          <Route path="/watch-later" element={<WatchLater />} />
           <Route path="*" element={<h1 className="not-found">Page Not Found</h1>} />
         </Routes>
       </div>
---
+++ src/components/Modal.jsx
@@ -0,0 +1,67 @@
+import React,{useEffect, useState}from 'react'
+import YouTubePlayer from "./YoutubePlayer";
+import { useDispatch, useSelector } from 'react-redux'
+import {closeModal} from '../data/modalSlice'
+import {starMovie, unstarMovie} from '../data/starredSlice'
+import {addToWatchLater} from '../data/watchLaterSlice'
+
+import { MdClose } from "react-icons/md"
+import '../styles/Modal.css'
+
+
+const Modal = () => {
+    const dispatch = useDispatch();
+    const { movieTrailerModal,videoKey, movie} = useSelector((state) => state.modal);
+
+    const handleCloseModal = () => {
+        dispatch(closeModal())
+    };
+
+    const handleAddFavourites=()=>{
+      dispatch(starMovie({
+        id: movie.id, 
+        overview: movie.overview, 
+        release_date: movie.release_date?.substring(0, 4),
+        poster_path: movie.poster_path,
+        title: movie.title
+      }))
+    }
+    const handleAddWatchLater=()=>{
+      dispatch(addToWatchLater({
+        id: movie.id, 
+        overview: movie.overview, 
+        release_date: movie.release_date?.substring(0, 4),
+        poster_path: movie.poster_path,
+        title: movie.title
+      }))
+    }
+   
+    
+
+
+  return (
+    <div className='movie-modal'>
+      
+      <MdClose className="close-modal-btn" onClick={handleCloseModal}/>
+         {videoKey && movieTrailerModal? (
+        <>
+          <YouTubePlayer videoKey={videoKey} />
+          <p className='overview'>
+            {movie.overview}
+          </p>
+
+        </>
+        ) : (
+          <div style={{ padding: "30px" }}>
+            <h6>no trailer available. Try another movie</h6>
+          </div>
+        )}
+        <div className="actions">
+          <button className='watch-later' onClick={handleAddWatchLater}>Watch Later</button>
+          <button className='add-favourites' onClick={handleAddFavourites}>Add to Favourites</button>
+        </div>
+    </div>
+  )
+}
+
+export default Modal
