f7ccda4c62a07c1ed45398e1a13160cfe77555c5
Kevin Amexo <bangitskev@gmail.com>
2024-02-16 11:49:31 +0000

    feat(style): add grid styling to component

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,11 +1,12 @@
 import Movie from './Movie'
 import '../styles/movies.scss'
+import '../styles/Movies.css'
 
 const Movies = ({ movies, viewTrailer, closeCard }) => {
 
     return (
-        <div data-testid="movies">
-            {movies.movies.results?.map((movie) => {
+        <div data-testid="movies" className="movies">
+            {movies.map((movie) => {
                 return (
                     <Movie 
                         movie={movie} 
