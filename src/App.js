82f7306fefe63c5cd48cbc5472effab28a1eaf80
Kevin Amexo <bangitskev@gmail.com>
2024-02-19 22:55:33 +0000

    Refactor: Apply formatting changes for consistency and readability

--- src/App.js
+++ src/App.js
@@ -1,80 +1,116 @@
-import { useEffect, useRef, useState,useCallback } from 'react'
-import { Routes, Route, createSearchParams, useSearchParams, useNavigate } from "react-router-dom"
-import { useDispatch, useSelector } from 'react-redux'
-import 'reactjs-popup/dist/index.css'
-import { fetchMovies, setPage,setMovies } from './data/moviesSlice'
-import { ENDPOINT_SEARCH, ENDPOINT_DISCOVER, ENDPOINT, API_KEY } from './constants'
-import Header from './components/Header'
-import Movies from './components/Movies'
-import Starred from './components/Starred'
-import WatchLater from './components/WatchLater'
-import Modal from './components/Modal'
-import './app.scss'
+import { useEffect, useRef, useCallback } from "react";
+import {
+  Routes,
+  Route,
+  useSearchParams,
+  useNavigate,
+} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import "reactjs-popup/dist/index.css";
+import { fetchMovies, setPage, setMovies } from "./data/moviesSlice";
+import {
+  ENDPOINT_SEARCH,
+  ENDPOINT_DISCOVER,
+} from "./constants";
+import Header from "./components/Header";
+import Movies from "./components/Movies";
+import Starred from "./components/Starred";
+import WatchLater from "./components/WatchLater";
+import Modal from "./components/Modal";
+import "./app.scss";
 
 const App = () => {
-
   const { movieTrailerModal, videoKey } = useSelector((state) => state.modal);
-  const dispatch = useDispatch()
-  const [searchParams, setSearchParams] = useSearchParams()
-  const searchQuery = searchParams.get('search')
-  const [isOpen, setOpen] = useState(false)
-  const shouldFetch=useRef(true)
-  const navigate = useNavigate()
-  const {searchResults, movies, page} =useSelector(state=>state.movies)
+  const dispatch = useDispatch();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchQuery = searchParams.get("search");
 
-  
-  const closeModal = () => setOpen(false)
-  
-  // const closeCard = () => {
-
-  // }
-
+  const shouldFetch = useRef(true);
+  const navigate = useNavigate();
+  const { searchResults, movies, page, searchTerm } = useSelector(
+    (state) => state.movies
+  );
 
   const searchMovies = (query) => {
-    navigate('/')
-    getMovies(shouldFetch.current)
-  }
+    navigate("/");
+    getMovies(shouldFetch.current);
+  };
 
-  const getMovies = useCallback((initialFetch) => {   
-    dispatch(fetchMovies({ apiUrl: ENDPOINT_DISCOVER, page, initialFetch: initialFetch === true ? initialFetch : false, type: 'discover' }));
-  }, [dispatch, searchQuery, page]);
+  const getMovies = useCallback(
+    (initialFetch) => {
+      dispatch(
+        fetchMovies({
+          apiUrl: ENDPOINT_DISCOVER,
+          page,
+          initialFetch: initialFetch === true ? initialFetch : false,
+          type: "discover",
+        })
+      );
+    },
+    [dispatch, searchQuery, page]
+  );
 
-  const getSearchResults = useCallback((initialFetch) => {    
-    dispatch(fetchMovies({apiUrl:`${ENDPOINT_SEARCH}&query=${searchQuery}`, page:page+1, initialFetch: initialFetch === true ? initialFetch : false, type: 'search' }));
-  }, [dispatch, searchQuery, page]);
-
-  
+  const getSearchResults = useCallback(
+    (initialFetch) => {
+      dispatch(
+        fetchMovies({
+          apiUrl: `${ENDPOINT_SEARCH}&query=${searchQuery}`,
+          page: page + 1,
+          initialFetch: initialFetch === true ? initialFetch : false,
+          type: "search",
+        })
+      );
+    },
+    [dispatch, searchQuery, page]
+  );
 
   useEffect(() => {
-    if(shouldFetch.current){
-      shouldFetch.current=false
-      getMovies(shouldFetch.current)
+    if (shouldFetch.current) {
+      shouldFetch.current = false;
+      getMovies(shouldFetch.current);
     }
 
-    return ()=>{
-      dispatch(setPage(1))
-      setMovies([])
-    }
-  }, [])
+    return () => {
+      dispatch(setPage(1));
+      setMovies([]);
+    };
+  }, []);
   return (
-    <div className={`App ${movieTrailerModal ===true && "full"}`}>
-      <Header searchMovies={searchMovies} searchParams={searchParams} setSearchParams={setSearchParams} initialFetch={shouldFetch.current} />
-
+    <div className={`App ${movieTrailerModal === true && "full"}`}>
+      <Header
+        searchMovies={searchMovies}
+        searchParams={searchParams}
+        setSearchParams={setSearchParams}
+        initialFetch={shouldFetch.current}
+      />
       <div className="container">
-         {movieTrailerModal&&(
+        {movieTrailerModal && (
           <div className="modal-container">
-              <Modal videoKey={videoKey}/>
+            <Modal videoKey={videoKey} />
           </div>
-          )}
+        )}
         <Routes>
-          <Route path="/" element={<Movies movies={searchResults.length? searchResults:movies}  closeCard={closeCard} initialFetch={shouldFetch.current} searchQuery={searchQuery} fetchMore={searchQuery? getSearchResults:getMovies}/>} />
-          <Route path="/starred" element={<Starred  />} />
+          <Route
+            path="/"
+            element={
+              <Movies
+                movies={searchTerm ? searchResults : movies}
+                initialFetch={shouldFetch.current}
+                searchQuery={searchQuery}
+                fetchMore={searchQuery ? getSearchResults : getMovies}
+              />
+            }
+          />
+          <Route path="/starred" element={<Starred />} />
           <Route path="/watch-later" element={<WatchLater />} />
-          <Route path="*" element={<h1 className="not-found">Page Not Found</h1>} />
+          <Route
+            path="*"
+            element={<h1 className="not-found">Page Not Found</h1>}
+          />
         </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,100 +1,103 @@
-import { Link, NavLink, useNavigate } from "react-router-dom"
-import { useDispatch, useSelector } from 'react-redux'
-import useDebounce from '../hooks/useDebounce'
-import '../styles/header.scss'
-import { useEffect,useState} from "react"
-import { fetchMoviesByQuery} from "../api/moviesApi"
-import {setSearchResults, setPage} from '../data/moviesSlice'
-import { useSearchParams } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import useDebounce from "../hooks/useDebounce";
+import "../styles/header.scss";
+import { useEffect, useState } from "react";
+import { fetchMoviesByQuery } from "../api/moviesApi";
+import { setSearchResults, setPage, setSearchTerm } from "../data/moviesSlice";
+import { useSearchParams } from "react-router-dom";
 import { CiTimer } from "react-icons/ci";
-import useScreenWidth from '../hooks/useScreenWidth';
+import useScreenWidth from "../hooks/useScreenWidth";
 
-
-const Header = ({initialFetch}) => {
-  
-  const { starredMovies } = useSelector((state) => state.starred)
-  const { page } = useSelector((state) => state.movies)
-  const dispatch= useDispatch()
-  const navigate= useNavigate()
-  const [searchTerm, setSearchTerm] = useState('');
-  const debouncedSearchTerm= useDebounce(searchTerm,300)
+const Header = ({ initialFetch }) => {
+  const { starredMovies } = useSelector((state) => state.starred);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [searchInputValue, setSearchInputValue] = useState("");
+  const debouncedSearchTerm = useDebounce(searchInputValue, 300);
   const [searchParams, setSearchParams] = useSearchParams();
   const screenWidth = useScreenWidth();
 
+  const getSearchResults = async () => {
+    const searchResults = await fetchMoviesByQuery(debouncedSearchTerm);
+    dispatch(setSearchResults({ movies: searchResults }));
+    return searchResults;
+  };
 
-
-  const getSearchResults= async()=>{
-    const searchResults= await fetchMoviesByQuery(debouncedSearchTerm)
-    dispatch(setSearchResults({movies:searchResults}))
-    return searchResults
-  }
-
-
-  useEffect(()=>{
-    navigate('/')
-    dispatch(setPage(1))
-    if(!debouncedSearchTerm.length>0 && initialFetch===false){
-     
-      setSearchTerm('')
-      dispatch(setSearchResults({movies:[]}))
-     
-    }else {
-      setSearchParams(debouncedSearchTerm?{ search: debouncedSearchTerm }:{});
-      getSearchResults()
-      
+  useEffect(() => {
+    navigate("/");
+    dispatch(setPage(1));
+    dispatch(setSearchTerm(debouncedSearchTerm));
+    if (!debouncedSearchTerm.length > 0 && initialFetch === false) {
+      setSearchInputValue("");
+      dispatch(setSearchResults({ movies: [] }));
+    } else {
+      setSearchParams(
+        debouncedSearchTerm ? { search: debouncedSearchTerm } : {}
+      );
+      getSearchResults();
     }
 
-    return()=>{
-      dispatch(setSearchResults({movies:[]}))
-      dispatch(setPage(1))
+    return () => {
+      dispatch(setSearchResults({ movies: [] }));
+      dispatch(setPage(1));
       setSearchParams({}); // Clear search parameters
-
-    }
-  },[debouncedSearchTerm])
-
-
-  
+    };
+  }, [debouncedSearchTerm]);
 
   return (
     <header>
-      <Link to="/" data-testid="home" onClick={() => {
-        setSearchTerm('')
-      }
-      }>
+      <Link
+        to="/"
+        data-testid="home"
+        onClick={() => {
+          setSearchInputValue("");
+        }}
+      >
         <i className="bi bi-film" />
       </Link>
 
       <div className="search-input-group rounded">
-          <input type="search" data-testid="search-movies"
-            onChange={(e)=>{
-              window.scrollTo(0, 0);
-              setSearchTerm(e.target.value)
-            }}
-            value={searchTerm}
-            className="form-control rounded" 
-            placeholder="Search movies..." 
-            aria-label="Search movies" 
-            aria-describedby="search-addon" 
-            />
-      </div>   
+        <input
+          type="search"
+          data-testid="search-movies"
+          onChange={(e) => {
+            window.scrollTo(0, 0);
+            setSearchInputValue(e.target.value);
+          }}
+          value={searchInputValue}
+          className="form-control rounded"
+          placeholder="Search movies..."
+          aria-label="Search movies"
+          aria-describedby="search-addon"
+        />
+      </div>
 
-       <nav>
-        <NavLink to="/starred" data-testid="nav-starred" className="nav-starred">
+      <nav>
+        <NavLink
+          to="/starred"
+          data-testid="nav-starred"
+          className="nav-starred"
+        >
           {starredMovies.length > 0 ? (
             <>
-            <i className="bi bi-star-fill bi-star-fill-white" />
-            <sup className="star-number">{starredMovies.length}</sup>
+              <i className="bi bi-star-fill bi-star-fill-white" />
+              <sup className="star-number">{starredMovies.length}</sup>
             </>
           ) : (
             <i className="bi bi-star" />
           )}
         </NavLink>
         <NavLink to="/watch-later" className="nav-fav">
-          {screenWidth>480? "watch later": <CiTimer  className="header-icon"/>}
+          {screenWidth > 480 ? (
+            "watch later"
+          ) : (
+            <CiTimer className="header-icon" />
+          )}
         </NavLink>
-      </nav>   
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
